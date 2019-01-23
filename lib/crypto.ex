9e48d0542b7d78d0f8974a8ff8f0c0440dc68700
Kristian Larsson <kristian@tutamail.com>
2019-01-22 21:27:23 -0800

    added add_hash function

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -6,10 +6,14 @@ defmodule Crypto do
     |> Base.encode16()
   end
 
-  def hash(%Block{} = block) do
+  defp hash(%Block{} = block) do
     block
     |> Map.take(@hash_fields)
     |> Poison.encode!()
     |> sha256()
+  end
+
+  def add_hash(%{Block{} = block) do
+    Map.put(block, :hash, hash(block))
   end
 end
