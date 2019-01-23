e278479fa4b04f25d61146f92f58d12f999bdb3a
Kristian Larsson <kristian@tutamail.com>
2019-01-22 21:20:06 -0800

    added hashing of fields

---
+++ lib/crypto.ex
@@ -0,0 +1,15 @@
+defmodule Crypto do
+  @hash_fields [:data, :prev_hash, :timestamp]
+
+  defp sha256(binary) do
+    :crypto.hash(:sha256, binary)
+    |> Base.encode16()
+  end
+
+  def hash(%Block{} = block) do
+    block
+    |> Map.take(@hash_fields)
+    |> Poison.encode!()
+    |> sha256()
+  end
+end
