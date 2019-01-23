e1ad144fa5162d7917e85c7a249a6872f42e2aa1
Kristian Larsson <kristian@tutamail.com>
2019-01-22 16:14:43 -0800

    upgraded to elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SimpleBlockchain.MixProject do
     [
       app: :simple_blockchain,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
